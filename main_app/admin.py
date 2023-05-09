a8db09bc3e76273f4b9e82c0e7284afd2de3aad1
kearmododragon <ckearney1992@gmail.com>
2023-05-09 14:22:46 +0200

    continents loads on page and is built corrently with images

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Continent
 
 # Register your models here.
+admin.site.register(Continent)
--- main_app/models.py
+++ main_app/models.py
@@ -4,14 +4,23 @@ from django.db import models
 
 class Continent(models.Model):
     name = models.CharField(max_length=30)
-    image = models.ImageField
+    image = models.URLField(null=True, blank=True, max_length=5000)
+
 
 class Country(models.Model):
     name = models.CharField(max_length=300)
     continent = models.ForeignKey
+    image = models.URLField
     mapsURL = models.URLField
+    
+    def __str__(self):
+       return self.name
 
 class City(models.Model):
     name = models.CharField(max_length=300)
+    image = models.URLField
     country = models.ForeignKey
+
+    def __str__(self):
+        return self.name
 
