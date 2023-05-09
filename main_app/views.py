cf325ed3537d559ff856059ac1f3ff7c063fa445
kearmododragon <ckearney1992@gmail.com>
2023-05-09 11:54:01 +0200

    continents page built, no links

--- main_app/views.py
+++ main_app/views.py
@@ -3,12 +3,12 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
 continents = [
-   {"name": "Africa"},
-   {"name": "Asia"},
-   {"name": "Europe"},
-   {"name": "Australasia"},
-   {"name": "North America"},
-   {"name": "South America"},
+   {"name": "Africa", "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Africa_%28orthographic_projection%29.svg/640px-Africa_%28orthographic_projection%29.svg.png" },
+   {"name": "Asia", "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Asia_%28orthographic_projection%29.svg/1200px-Asia_%28orthographic_projection%29.svg.png"},
+   {"name": "Europe", "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Europe_orthographic_Caucasus_Urals_boundary_%28with_borders%29.svg/1200px-Europe_orthographic_Caucasus_Urals_boundary_%28with_borders%29.svg.png"},
+   {"name": "Oceana", "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Oceania_%28centered_orthographic_projection%29.svg/1200px-Oceania_%28centered_orthographic_projection%29.svg.png"},
+   {"name": "North America", "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Location_North_America.svg/1200px-Location_North_America.svg.png"},
+   {"name": "South America", "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/South_America_%28orthographic_projection%29.svg/1200px-South_America_%28orthographic_projection%29.svg.png"},
 ]
 
 # Create your views here.
