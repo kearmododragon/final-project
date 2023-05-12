44f463251cf1ea00b1f0f4cc917fed4be827282e
kearmododragon <ckearney1992@gmail.com>
2023-05-12 23:21:15 +0200

    deployment check

--- holidaying/settings.py
+++ holidaying/settings.py
@@ -81,10 +81,15 @@ STATICFILES_DIRS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'holiday',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'competivie-holiday',
+        'USER': 'holidayingadmin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
