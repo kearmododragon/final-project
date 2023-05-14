ef7e98d90ea9471a0cb6208306b61ca78b1adbe0
kearmododragon <ckearney1992@gmail.com>
2023-05-14 22:42:54 +0200

    login + signup changed and hopefully server reverted

--- main_app/forms.py
+++ main_app/forms.py
@@ -14,4 +14,4 @@ class CityForm(ModelForm):
 class HolidayForm(ModelForm):
   class Meta:
     model = Holiday
-    fields =["name", "cities", "companions",]
+    fields =["name", "city", "companions",]
